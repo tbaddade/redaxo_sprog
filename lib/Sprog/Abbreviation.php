185f1ef125936764bedeafc83a45bd3a37cefe62
tbaddade <thomas@redaxo.org>
2023-01-05 12:08:28 +0100

    Abbr - Ersetzungen nur im body-Tag

--- lib/Sprog/Abbreviation.php
+++ lib/Sprog/Abbreviation.php
@@ -19,16 +19,23 @@ class Abbreviation
             $clangId = \rex_clang::getCurrentId();
         }
 
+        preg_match_all('|<body[^>]*>(.*)</body>|msU', $content, $body);
+
+        if (!isset($body[1][0])) {
+            return $content;
+        }
+        $bodyReplace = $body[1][0];
+
         $sql = \rex_sql::factory();
         $sql->setQuery('SELECT `abbreviation`, `text` FROM '.\rex::getTable('sprog_abbreviation').' WHERE `clang_id` = :clangId AND `status` = 1', ['clangId' => $clangId]);
         $items = $sql->getArray();
 
         foreach ($items as $item) {
-            $content = preg_replace_callback('|(?!<[^<>]*?)(?<![?.&])\b'.$item['abbreviation'].'\b(?!:)(?![^<>]*?>)|msU', function ($matches) use ($item) {
+            $bodyReplace = preg_replace_callback('|(?!<[^<>]*?)(?<![?.&])\b'.$item['abbreviation'].'\b(?!:)(?![^<>]*?>)|msU', function ($matches) use ($item) {
                     return sprintf('<abbr title="%s">%s</abbr>', rex_escape($item['text']), $matches[0]);
-            }, $content);
+            }, $bodyReplace);
         }
 
-        return $content;
+        return str_replace($body[1][0], $bodyReplace, $content);
     }
 }
